c997372427333be3e8ced276eacdd0751bc530c4
Maksim <brazhmax@mail.ru>
2022-09-04 16:21:39 +0300

    add Task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,73 @@
+﻿/* Задача 54. Задайте двумерный массив. Напишите программу, которая упорядочит по 
+убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2  */
+
+using static System.Console;
+Clear();
+
+int m = InputNumbers("Введите количество строк в массиве: ");
+int n = InputNumbers("Введите количество столбцов в массиве: ");
+int range = InputNumbers("Введите диапазон от 0 до 10: ");
+
+int[,] array = new int[m, n];
+NewArray(array);
+WriteArray(array);
+WriteLine($"Отсортированный массив: ");
+ArrayLines(array);
+WriteArray(array);
+
+void ArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Write(input);
+    int output = Convert.ToInt32(ReadLine());
+    return output;
+}
+
+void NewArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        WriteLine();
+    }
+}
